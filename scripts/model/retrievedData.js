b08a9bfdac36be958fadba1cc02a2d09547a0188
meganrm <meganrm@gmail.com>
2016-09-01 09:20:37 -0700

    saved the api call for updating a file

--- scripts/model/retrievedData.js
+++ scripts/model/retrievedData.js
@@ -33,6 +33,28 @@
     };
   };
 
+
+  // $.ajax({
+  //    url:'https://api.github.com/repos/meganrm/301-portfolio/contents/scripts/test.js',
+  //    method: 'PUT',
+  //    data: '{"message": "trying to add a new file via api", "content": "aGVsbG8=","branch": "master"}',
+  //    headers: {Authorization: 'token ' + 'f27c0c66c56d1d75e91d40b5097f2b84a8edcc72'},
+  //    success: function(data, message, xhr){
+  //       console.log(data)
+  //     }
+  //   })
+  //
+  //   $.ajax({
+  //   url:'https://api.github.com/repos/meganrm/301-portfolio/contents/scripts/test.js',
+  //   method: 'PUT',
+  //   data: '{"message": "trying to add a new file via api", "content": "aGVsbG8=","branch": "master"}',
+  //   headers: {Authorization: 'token ' + 'f27c0c66c56d1d75e91d40b5097f2b84a8edcc72'},
+  //   success: function(data, message, xhr){
+  //      console.log(data)
+  //    }
+  //  })
+
+
   console.log('removed recursive');
   module.GetData = GetData;
 })(window);
